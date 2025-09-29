32b3b572e9759e63b8e2f4402dbce4f407b148f8
Parven <parven5@proton.me>
2025-09-29 20:24:10 +0800

    Hello Triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,123 @@
-#include <SDL3/SDL.h>
 #include <GL/glew.h>
+#include <SDL3/SDL.h>
 #include <iostream>
 
+// #include <fstream>
+// #include <sstream>
+// #include <string>
+
+// std::string ReadTextFile(const std::string &filename) {
+//   std::ifstream file(filename);
+//   if (!file.is_open()) {
+//     return "";
+//   }
+//   std::stringstream ss{};
+//   ss << file.rdbuf();
+//   file.close();
+
+//   return ss.str();
+// }
+
+const char *vertexShaderSource =
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+const char *fragmentShaderSource =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
+
 int main() {
-    if (SDL_Init(SDL_INIT_VIDEO) <= 0) {
-        std::cerr << "SDL Init Error: " << SDL_GetError() << std::endl;
-        return 1;
+  if (SDL_Init(SDL_INIT_VIDEO) <= 0) {
+    std::cerr << "SDL Init Error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+  SDL_Window *window =
+      SDL_CreateWindow("OpenGL Window", 800, 600, SDL_WINDOW_OPENGL);
+  if (!window) {
+    std::cerr << "SDL CreateWindow Error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+
+  SDL_GLContext glContext = SDL_GL_CreateContext(window);
+  if (!glContext) {
+    std::cerr << "OpenGL Context Error: " << SDL_GetError() << std::endl;
+    return 1;
+  }
+
+  if (glewInit() != GLEW_OK) {
+    std::cerr << "Failed to init GLEW\n";
+    return 1;
+  }
+
+  unsigned int vertexShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  unsigned int fragmentShader;
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  float vertices[] = {-0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
+                      0.0f,  0.0f,  0.5f, 0.0f
+
+  };
+
+  unsigned int VAO, VBO;
+  glGenVertexArrays(1, &VAO);
+  glBindVertexArray(VAO);
+
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  bool running = true;
+  SDL_Event event;
+  while (running) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+        running = false;
+      }
     }
 
-    SDL_Window* window = SDL_CreateWindow("OpenGL Window",
-                                          800, 600,
-                                          SDL_WINDOW_OPENGL);
-    if (!window) {
-        std::cerr << "SDL CreateWindow Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-    SDL_GLContext glContext = SDL_GL_CreateContext(window);
-    if (!glContext) {
-        std::cerr << "OpenGL Context Error: " << SDL_GetError() << std::endl;
-        return 1;
-    }
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
 
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "Failed to init GLEW\n";
-        return 1;
-    }
+    SDL_GL_SwapWindow(window);
+  }
 
-    bool running = true;
-    SDL_Event event;
-    while (running) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_EVENT_QUIT) {
-                running = false;
-            }
-        }
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
 
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        SDL_GL_SwapWindow(window);
-    }
-
-    SDL_GL_DestroyContext(glContext);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
+  SDL_GL_DestroyContext(glContext);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
 }
